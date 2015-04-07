005e24bf0ec24de64776cd57544ea87ae36637d2
simardj <Jeremy@MacBook-Pro-de-Jeremy.local>
2015-04-07 13:47:07 +0200

    fixe index.php

--- index.php
+++ index.php
@@ -1,30 +1,53 @@
 <?php
-
-	error_reporting()(E_ALL);
-	init_set("display_errors", 1);
-	
-	/* 
-	** Installation SDK FACEBOOK
-	*/
-
 	session_start();
-
+	//Installation SDK FACEBOOK
 	require("facebook-php-sdk-v4-4.0-dev/autoload.php");
-
-	use Faceook\FacebookSession;	
-	use Facebook\FacebookRedirectLoginHelper;	
-
 	const APPID = "1553479528238610";
 	const APPSECRET = "f5b2542a764c91a66ec31f768bc87025";
 
+	//Initialiser le PHP SDK
+	use Facebook\FacebookSession;
+	use Facebook\FacebookRequestException;
 	FacebookSession::setDefaultApplication(APPID, APPSECRET);
 
-	/* 
-	** Générer l'URL de connexion
-	*/
+	//Générer l'url de connexion
+	use Facebook\FacebookRedirectLoginHelper;
+	$redirectUrl = 'http://localhost/';
+	$helper = new FacebookRedirectLoginHelper($redirectUrl);
+	$loginUrl = $helper->getLoginUrl(['email, user_birthday']);
 
-	$helper = new FacebookRedirectLoginHelper("https://simard.herokuapp.com");
+	//Récupérer les informations de session Facebook et les associer à la session courante
 
+
+	if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	else {
+		$session = $helper->getSessionFromRedirect();
+	}
+
+	//Afficher le profil utilisateur
+	use Facebook\FacebookRequest;
+	use Facebook\GraphUser;
+
+	if($session) {
+
+  		try {
+
+   			$user_profile = (new FacebookRequest(
+      		$session, 'GET', '/me'
+    		))->execute()->getGraphObject(GraphUser::className());
+
+    		echo "Name: " . $user_profile->getName();
+
+  		} catch(FacebookRequestException $e) {
+
+    		echo "Exception occured, code: " . $e->getCode();
+    		echo " with message: " . $e->getMessage();
+
+  		}   
+
+	}
 
 ?>
 
@@ -37,13 +60,12 @@
 		<title>App Facebook</title>
 		<meta name="description" content="Page de test : App Facebook">
 
-		<!-- 
-			Connexion JavaScript (Plugins)
-		-->
+		<!-- Setup the Facebook SDK for JavaScript -->
+
 		<script>
 		  window.fbAsyncInit = function() {
 		    FB.init({
-		      appId      : '<?php echo APPID; ?>',
+		      appId      : '1553479528238610',
 		      xfbml      : true,
 		      version    : 'v2.3'
 		    });
@@ -56,29 +78,24 @@
 		     js.src = "//connect.facebook.net/fr_FR/sdk.js";
 		     fjs.parentNode.insertBefore(js, fjs);
 		   }(document, 'script', 'facebook-jssdk'));
+
 		</script>		
 	</head>
 
 	<body>
-		<!--
-			Bouton J'aime / Partager
-		-->
+		
+		
+
+		<!-- Testez votre intégration à Facebook -->
+
 		<div
 		  class="fb-like"
 		  data-share="true"
 		  data-width="450"
 		  data-show-faces="true">
 		</div>
-
-		<!--
-			Commentaires
-		-->
+		</br>
 		<div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-numposts="5" data-colorscheme="light"></div>
-
-		<?php
-			$loginUrl = $helper->getLoginUrl();
-			echo '<a href="'.$loginUrl.'">Se connecter</a>';
-		?>
 
 	</body>
 
