18d2988efc3181f9c7d34037c237ba2335e2857c
simardj <Jeremy@MacBook-Pro-de-Jeremy.local>
2015-04-08 15:13:12 +0200

    ajout connexion

--- index.php
+++ index.php
@@ -1,60 +1,46 @@
 <?php
-
-	session_start();
-
-	//Installation SDK FACEBOOK
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    session_start();
 	require "facebook-php-sdk-v4-4.0-dev/autoload.php";
-	const APPID = "1553479528238610";
-	const APPSECRET = "f5b2542a764c91a66ec31f768bc87025";
-
-	//Initialiser le PHP SDK
 	use Facebook\FacebookSession;
-	use Facebook\FacebookRequestException;
-	FacebookSession::setDefaultApplication(APPID, APPSECRET);
-
-	//Générer l'url de connexion
 	use Facebook\FacebookRedirectLoginHelper;
-	//$redirectUrl = 'http://localhost/';
-	$redirectUrl = 'https://simard.herokuapp.com';
-	$helper = new FacebookRedirectLoginHelper($redirectUrl);
-	$loginUrl = $helper->getLoginUrl();
-
-	//Récupérer les informations de session Facebook et les associer à la session courante
-
-
-	if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	else {
-		$session = $helper->getSessionFromRedirect();
-	}
-
-	//Afficher le profil utilisateur
 	use Facebook\FacebookRequest;
 	use Facebook\GraphUser;
-
+	const APPID = "1553479528238610";
+	const APPSECRET = "f5b2542a764c91a66ec31f768bc87025";
+	FacebookSession::setDefaultApplication(APPID, APPSECRET);
+    $helper = new FacebookRedirectLoginHelper('https://simard.herokuapp.com');
+	
+	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+	// alors je veux créer mon utilisateur à partir de cette session
+	if( isset($_SESSION) && isset($_SESSION['fb_token']) ) {
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	//Sinon j'affiche le lien de connection
+	else
+	{
+		$session = $helper->getSessionFromRedirect();
+	}
+	
 ?>
 
 <!DOCTYPE html>
-
 <html>
-
 	<head>
 		<meta charset="UTF-8">
-		<title>App Facebook</title>
-		<meta name="description" content="Page de test : App Facebook">
-
-		<!-- Setup the Facebook SDK for JavaScript -->
-
+		<title>Titre de ma page</title>
+		<meta name="description" content="contenu de ma page">
+	</head>
+	<body>
 		<script>
 		  window.fbAsyncInit = function() {
 		    FB.init({
-		      appId      : '1553479528238610',
+		      appId      : '<?php echo APPID; ?>',
 		      xfbml      : true,
 		      version    : 'v2.3'
 		    });
 		  };
-
 		  (function(d, s, id){
 		     var js, fjs = d.getElementsByTagName(s)[0];
 		     if (d.getElementById(id)) {return;}
@@ -62,15 +48,9 @@
 		     js.src = "//connect.facebook.net/fr_FR/sdk.js";
 		     fjs.parentNode.insertBefore(js, fjs);
 		   }(document, 'script', 'facebook-jssdk'));
+		</script>
 
-		</script>		
-	</head>
-
-	<body>
-
-		<h1>Mon application Facebook</h1>
-
-		<!-- Testez votre intégration à Facebook -->
+		<h1>Mon app FB</h1>
 
 		<div
 		  class="fb-like"
@@ -78,14 +58,9 @@
 		  data-width="450"
 		  data-show-faces="true">
 		</div>
-		</br>
-		<div class="fb-comments" data-href="https://simard.herokuapp.com" data-numposts="5" data-colorscheme="light"></div>
 
-
-		<div id="fb-root"></div>
-
+		<br>
 		<?php
-
 			if($session)
 			{
 				$_SESSION['fb_token'] = (string) $session->getAccessToken();
@@ -97,9 +72,8 @@
 				$loginUrl = $helper->getLoginUrl();
 				echo "<a href='".$loginUrl."'>Se connecter</a>";
 			}
-
 		?>
+		
 
 	</body>
-
 </html>
